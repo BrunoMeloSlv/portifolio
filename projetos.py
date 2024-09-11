a9e2ec1f97fdc48883b816694f9cd1c7a1fd527f
Bruno Melo da Silva <91537585+BrunoMeloSlv@users.noreply.github.com>
2024-09-11 15:28:13 -0300

    Add files via upload

--- projetos.py
+++ projetos.py
@@ -5,7 +5,7 @@ def run():
     st.write("Aqui estão alguns dos meus projetos de análise de dados no GitHub. Cada repositório inclui um README detalhado e, em alguns casos, vídeos explicativos no YouTube.")
     st.write("[Projeto 1 - Análise de Aprovação de Crédito](https://github.com/BrunoMeloSlv/Credit-Approval)")
     st.write("[Projeto 2 - Análise de Aprovação de Churn](https://github.com/BrunoMeloSlv/Adeus-Clientes-O-que-Faz-o-Varejo-Online-Perder-Clientes-)")
-    st.write("[Projeto 3 - Análise do Campeonato Brasileiro](https://appbrasileirao2024.streamlit.app/)")
+    st.write("[Projeto 3 - Análise do Campeonato Brasileiro](https://github.com/BrunoMeloSlv/app_brasileirao_2024)")
     st.write("[Projeto 4 - Uma Análise de Correspondência dos Top 100 movies IMDb](https://github.com/BrunoMeloSlv/top-100-movies)")
     st.write("[Projeto 5 - WhatsApp Web Auto-Messenger](https://github.com/BrunoMeloSlv/Mensageria_WhatsApp)")
     st.write("[Projeto 6 - Análise de Clustering Spotify](https://github.com/BrunoMeloSlv/analise-de-clustering-spotify)")
